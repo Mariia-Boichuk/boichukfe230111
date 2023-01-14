70677cca3e246de340c2a1e824286076c9d32005
MariaBoi <akivene55@gmail.com>
2023-01-14 23:59:09 +0300

    render eslint

--- src/components/UserRow/UserRow.test.tsx
+++ src/components/UserRow/UserRow.test.tsx
@@ -13,30 +13,47 @@ const mockedData = {
   photo: "2.jpg",
 };
 
-describe("UserRow", () => {
-  beforeEach(() => {
-    render(<UserRow userData={mockedData} />);
-  });
+const setup = () => render(<UserRow userData={mockedData} />);
 
+describe("UserRow", () => {
   it("should render  without errors", () => {
+    setup();
+
     const row = screen.getByTestId("userInfoRow");
     expect(row).toBeInTheDocument();
   });
 
   it("should render btn without errors", () => {
+    setup();
+
     const name = screen.getByText(/Brazil Izair/i);
     expect(name).toBeInTheDocument();
   });
 
   it("should render nickname without errors", () => {
+    setup();
+
     const nickname = screen.getByText(/brazilizair/i);
     expect(nickname).toBeInTheDocument();
   });
 
   it("should render widget after clicking button", async () => {
+    setup();
+
     const Btn = screen.getByText(/view/i);
     await userEvent.click(Btn);
     const widg = screen.getByTestId("widget");
     expect(widg).toBeInTheDocument();
+  });
+
+  it("should close widget after clicking close button", async () => {
+    setup();
+
+    const Btn = screen.getByText(/view/i);
+    await userEvent.click(Btn);
+    const widg = screen.getByTestId("widget");
+    const closeBtn = screen.getByText(/x/i);
+    await userEvent.click(closeBtn);
+    expect(widg).not.toBeInTheDocument();
   });
 });
